e36458c3a11c95b165d888be53070ee06ee4b3e9
Viktorinho44 <xmalyv@stuba.sk>
2023-02-22 09:08:08 +0100

    Hi first push

---
+++ src/main/java/org/example/CvikoOne.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class CvikoOne {
+
+    public int compare( int a, int b){
+        if(a>b){
+            return 1;
+        }
+        else if(a==b){
+            return 0;
+        }
+        else{return -1;}
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        CvikoOne first = new CvikoOne();
+        System.out.println(first.compare(3,2));
+    }
+
+}
